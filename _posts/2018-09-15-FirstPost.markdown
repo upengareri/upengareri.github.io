a733b19c621dff89592212006a3589e6f3ee0fdd
Upendar Gareri <upendargareri@Upendars-Air.fritz.box>
2018-09-17 20:03:51 +0200

    Theme customization

---
+++ _posts/2018-09-15-FirstPost.markdown
@@ -0,0 +1,6 @@
+---
+layout: single
+title:  "Hello World"
+date:   2018-09-15
+---
+This is my first blog post. And today is engineer's day so I just wanna say "Ghanta Engineering" to all.
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -3,4 +3,9 @@ var store = [{
         "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": [],
         "tags": [],
         "url": "http://localhost:4000/welcome-to-jekyll/",
+        "teaser":null},{
+        "title": "Hello World",
+        "excerpt":"This is my first blog post. And today is engineer’s day so I just wanna say “Ghanta Engineering” to all. ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/FirstPost/",
         "teaser":null}]
