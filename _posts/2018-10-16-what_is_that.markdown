51bc7cfeb7e80661ee3b80d9969dae96bfa21b27
Upendar Gareri <upendargareri@Upendars-Air.fritz.box>
2018-10-16 16:38:22 +0200

    added new post - what_is_that

---
+++ _posts/2018-10-16-what_is_that.markdown
@@ -0,0 +1,28 @@
+---
+layout: single
+title:  "What is that?"
+date:   2018-10-16
+---
+
+Some terms and jargons.
+
+### Numpy:
+&emsp;` library with fast multi-dimensional array processing capabilities, a container for data`
+
+### Pandas:
+&emsp;` library that provides data structures and functions to interact with structured and tabular data in an easy and fast way`
+
+### Matplotlib: 		
+&emsp;` library for plotting and visualising two-dimensional data`
+
+### Ipython and Jupyter:
+&emsp;` for data exploration and visualisation`
+
+### Munge/Munging/Wrangling: 		
+&emsp;` the process of manipulating unstructured or messy data into a structured or clean form`
+
+### Pseudocode:
+&emsp;` describes an algorithm or process that takes a code like form, usually to describe a logic or flow`
+
+### Syntactic sugar: 		
+&emsp;` syntax added to make something easier or convenient to type (does not add feature)`
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -3,4 +3,9 @@ var store = [{
         "excerpt":"What is tmux? It stands for terminal multiplexer. In simple terms, it can multiply a single terminal window into multiple windows. Benefits It lets us run and keep an eye on multiple programs at once. For example, in one window we can run some python code and in another window,...","categories": [],
         "tags": [],
         "url": "http://localhost:4000/basics_of_tmux/",
+        "teaser":null},{
+        "title": "What is that?",
+        "excerpt":"Some terms and jargons. Numpy:   library with fast multi-dimensional array processing capabilities, a container for data Pandas:   library that provides data structures and functions to interact with structured and tabular data in an easy and fast way Matplotlib:   library for plotting and visualising two-dimensional data Ipython and...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/what_is_that/",
         "teaser":null}]
